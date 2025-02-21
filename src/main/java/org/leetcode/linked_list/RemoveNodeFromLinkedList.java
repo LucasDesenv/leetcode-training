8fb39d863d5416c2f133c9a89c29885a44539e8f
lucasDesenv <7883005+LucasDesenv@users.noreply.github.com>
2025-02-21 14:51:08 +0100

    More of LinkedList removing all repeated nodes, keeping only distinct.

--- src/main/java/org/leetcode/linked_list/RemoveNodeFromLinkedList.java
+++ src/main/java/org/leetcode/linked_list/RemoveNodeFromLinkedList.java
@@ -1,5 +1,8 @@
 package org.leetcode.linked_list;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
  */
@@ -14,13 +17,67 @@ public class RemoveNodeFromLinkedList {
     ListNode listNode = removeNthFromEnd(head, 2);
     System.out.println(listNode);
     ListNode listNode1 = new ListNode(1);
-    listNode1.next = new ListNode(2);
-    listNode1.next.next = new ListNode(7);
-    listNode1.next.next.next = new ListNode(8);
-    System.out.println(removeElements(listNode1, 7));
+    listNode1.next = new ListNode(1);
+    listNode1.next.next = new ListNode(1);
+    listNode1.next.next.next = new ListNode(3);
+    listNode1.next.next.next.next = new ListNode(3);
+    listNode1.next.next.next.next.next = new ListNode(4);
+    listNode1.next.next.next.next.next.next = new ListNode(9);
+    ListNode x = deleteDuplicates_V2(listNode1);
+    System.out.println(x);
 
 
   }
+
+  /**
+   * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/submissions/1550764882/?envType=problem-list-v2&envId=linked-list
+   * @param head
+   * @return
+   */
+  public static ListNode deleteDuplicates_V2(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode dummy = new ListNode(0, head);
+    ListNode prev = dummy;
+    ListNode cur = head;
+
+    while (cur != null) {
+      if (cur.next != null && cur.val == cur.next.val) {
+        int duplicatedValue = cur.val;
+        while (cur != null && duplicatedValue == cur.val) {
+          cur = cur.next; //keep skipping the duplicated values
+        }
+        prev.next = cur; //once a unique value is found, store it.
+      }else{
+        prev = cur; //it means the current and next are different, so we can move safely.
+        cur = cur.next;
+      }
+    }
+
+    return dummy.next;
+  }
+
+  public static ListNode deleteDuplicates(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode current = head;
+
+
+    while (current != null && current.next != null){
+      if (current.val == current.next.val) {
+        current.next = current.next.next;
+      }else {
+        current = current.next;
+      }
+    }
+
+    return head;
+  }
+
 
   public static ListNode removeElements(ListNode head, int val) {
     //First cover the corner case where we have all elements equal to val. E.g.: 7, 7, 7, 7
